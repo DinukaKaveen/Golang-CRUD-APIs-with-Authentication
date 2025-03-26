1f1fb710224e835acc41f7a1973526eaf0f65843
DinukaKaveen <dinukadominguhewa@gmail.com>
2025-03-26 16:10:58 +0530

    install fiber and gorm

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 import (
-	"fmt"
-	"net/http"
+	//"fmt"
+	//"net/http"
+    "github.com/gofiber/fiber/v2"
 )
 
+/*
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Hello, Docker")
 }
@@ -12,4 +14,15 @@ func main() {
     http.HandleFunc("/", handler)
     fmt.Println("Server started on port 8080")
     http.ListenAndServe(":8080", nil)
+}
+*/
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World!!, This is Go-Fiber Tutorial Series")
+	})
+
+	app.Listen(":8080")
 }
